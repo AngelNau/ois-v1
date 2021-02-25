d835b7a3b90d3c6fca687c720020457a76d350ad
Jurij Plaskan <jp0710@student.uni-lj.si>
2021-02-25 11:10:37 +0100

    63200221.jpg

---
+++ Gravitacija.java
@@ -0,0 +1,10 @@
+public class Gravitacija{
+    public static void main(String[] args){
+
+    }
+
+    public static void izpis(double nd, double a){
+        System.out.println(nd);
+        System.out.println(a);
+    }
+}
